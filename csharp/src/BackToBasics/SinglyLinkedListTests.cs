6f4ab8db3b1c6e231e5ca65a8f265152f6f223f3
Chris O'Dell <christine.ann.odell@gmail.com>
2013-12-01 14:59:13 +0000

    Added a first test to ensure each node contains a value

---
+++ csharp/src/BackToBasics/SinglyLinkedListTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace BackToBasics
+{
+	[TestFixture]
+    public class SinglyLinkedListTests
+    {
+		[Test]
+		public void Node_should_contain_value()
+		{
+			var node = new Node<int>(12);
+			Assert.That(node.Value, Is.EqualTo(12));
+		}
+    }
+
+	public class Node<T>
+	{
+		public int Value { get; set; }
+
+		public Node(int value)
+		{
+			Value = value;
+		}
+	}
+}
