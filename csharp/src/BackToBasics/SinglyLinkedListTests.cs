99126d3e00376a7c09022f688a68b1d1708a780c
Chris O'Dell <christine.ann.odell@gmail.com>
2013-12-01 15:36:25 +0000

    Added test to ensure nodes reference the next node

--- csharp/src/BackToBasics/SinglyLinkedListTests.cs
+++ csharp/src/BackToBasics/SinglyLinkedListTests.cs
@@ -11,11 +11,22 @@ namespace BackToBasics
 			var node = new Node<int>(12);
 			Assert.That(node.Value, Is.EqualTo(12));
 		}
-    }
+
+		[Test]
+		public void Node_should_contain_reference_to_next_node()
+		{
+			var node = new Node<int>(12);
+			var nextNode = new Node<int>(42);
+
+			node.Next = nextNode;
+			Assert.That(node.Next, Is.EqualTo(nextNode));
+		}
 
 	public class Node<T>
 	{
 		public int Value { get; set; }
+
+		public Node<T> Next { get; set; }
 
 		public Node(int value)
 		{
